67c7555258a08fcd72881d36a46a9390d0a76268
Phuc Vo <phucvo0709@gmail.com>
2020-03-06 15:58:23 +0700

    add broadcast

--- src/components/UI/AlertUI.js
+++ src/components/UI/AlertUI.js
@@ -6,33 +6,24 @@ import socketIo from "../../utils/socketIo";
 
 function AlertUI() {
   const dispatch = useDispatch();
-  const socket = useSelector(state => state.socket);
   const alert = useSelector(state => state.alert);
 
   socketIo.on("haveNewRooms", function(id) {
-    if (socket.socketId) {
-      if (socket.socketId !== id) {
-        dispatch(
-          onSetAlert({
-            type: "info",
-            text: "Have new Rooms"
-          })
-        );
-      }
-    }
+    dispatch(
+      onSetAlert({
+        type: "info",
+        text: "Have new Rooms"
+      })
+    );
   });
 
   socketIo.on("haveNewCustomers", function(id) {
-    if (socket.socketId) {
-      if (socket.socketId !== id) {
-        dispatch(
-          onSetAlert({
-            type: "info",
-            text: "Have new customers"
-          })
-        );
-      }
-    }
+    dispatch(
+      onSetAlert({
+        type: "info",
+        text: "Have new customers"
+      })
+    );
   });
 
   return (
